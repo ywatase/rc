2737652375664b50817fdbca646be67e65645b25
Yusuke Watase <ywatase@gmail.com>
2012-06-07 13:17:18 +0900

    delete perl-support.vim. then add perldoc.vim, perlomni.vim and so on

--- vim/bundle_local/apply_patch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-WORK_DIR=$(pwd)
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-
-cd $SCRIPT_DIR/../bundle/perl-support.vim/plugin/
-patch < $SCRIPT_DIR/perl-support/perl-support.vim.patch
-ln -s $HOME/.vim/bundle/perl-support.vim/perl-support/templates/idioms.template \
-    $HOME/.vim/bundle_local/perl-support/templates/idioms.template
