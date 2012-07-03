97057cebecbe2711bc1213ba47d25931b5ce0559
Yusuke Watase <ywatase@gmail.com>
2012-07-03 18:19:11 +0900

    add change-master.sql

---
+++ mysql/change-master.sql
@@ -0,0 +1,6 @@
+CHANGE MASTER TO
+    MASTER_HOST='master_host_name',
+	MASTER_USER='replication_user_name',
+	MASTER_PASSWORD='replication_password',
+	MASTER_LOG_FILE='recorded_log_file_name',
+	MASTER_LOG_POS=recorded_log_position;
