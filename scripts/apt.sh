bc5d87401889b1d66d58ada303b973c831fc847e
Yusuke Watase <ywatase@gmail.com>
2016-07-15 11:08:51 +0900

    add apt.sh

---
+++ scripts/apt.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install slim awesome
+sudo apt-get install zsh tmux vim-common
+# ruby
+sudo apt-get install build-essential bison openssl libreadline4 \
+   libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev \
+   libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf \
+   libc6-dev ncurses-dev
