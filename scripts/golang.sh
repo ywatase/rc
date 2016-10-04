8203995938edf2151f29f003fa4ad9e1a4cfff58
Yusuke Watase <ywatase@gmail.com>
2016-10-05 01:45:51 +0900

    golang.sh get go utilities.

---
+++ scripts/golang.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# goimports
+go get -u golang.org/x/tools/cmd/goimports
+
+# gocode
+go get -u github.com/nsf/gocode
+
+# godef
+go get -u github.com/rogpeppe/godef
+
+# golint
+go get -u github.com/golang/lint/golint
+
+# errcheck
+go get -u github.com/kisielk/errcheck
+
+# oracle
+go get -u golang.org/x/tools/cmd/oracle
