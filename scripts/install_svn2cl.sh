2e3b6071e4b3ac385501a7c33aa549ff3428501a
Yusuke Watase <ywatase@gmail.com>
2016-11-12 02:43:24 +0900

    R.I.P. old scripts

--- scripts/install_svn2cl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SVN2CL_VERSION=0.13
-INSTALL_DIR=~/bin/src
-[ -e $INSTALL_DIR ] || mkdir -p $INSTALL_DIR
-cd $INSTALL_DIR
-wget http://arthurdejong.org/svn2cl/svn2cl-${SVN2CL_VERSION}.tar.gz
-tar xzf svn2cl-${SVN2CL_VERSION}.tar.gz
-ln -s $INSTALL_DIR/svn2cl-${SVN2CL_VERSION}/svn2cl.sh ~/bin/svn2cl
