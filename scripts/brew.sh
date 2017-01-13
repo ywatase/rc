9babecd60dfbcdad745ea9358db05c16daf45c2b
Yusuke Watase <ywatase@gmail.com>
2017-01-13 20:30:29 +0900

    add vim

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -4,12 +4,16 @@ if ! [ -x "/usr/local/bin/brew" ] ; then
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+# High Priority
 while read pkg
 do
 	brew install $pkg
 done <<EOL
 zsh
 zsh-completions
+keychain
+readline
+tmux
 EOL
 
 while read tap
@@ -22,79 +26,7 @@ osx-cross/avr
 homebrew/dupes
 EOL
 
-while read pkg
-do
-	brew install $pkg
-done <<EOL
-ansible
-osx-cross/avr/avr-libc
-bsdmake
-cmake
-coreutils
-corkscrew
-ctags
-curl
-docker
-dfu-programmer
-gcc-arm-none-eabi
-gettext
-git
-git-flow
-gnu-sed
-gnu-tar
-go
-gobject-introspection
-groff
-harfbuzz
-htop-osx
-hub
-imagemagick
-innotop
-jmeter
-jq
-keychain
-libevent
-lynx
-memcached
-mongodb
-msgpack
-mysql
-ngrep
-nikto
-nkf
-nmap
-openldap
-openssh
-openssl
-packer
-peco
-percona-toolkit
-perl-build
-phantomjs
-pidof
-plenv
-proctools
-pstree
-pyenv
-qt
-rdesktop
-readline
-reattach-to-user-namespace
-redis
-rename
-rpm2cpio
-sqlite
-subversion
-the_silver_searcher
-tig
-tmux
-tree
-wakeonlan
-watch
-wget
-EOL
-
-
+# apps
 while read pkg
 do
 	brew cask install $pkg
@@ -108,4 +40,80 @@ qlmarkdown
 skype
 vagrant
 virtualbox
+EOL
+
+
+# utilities
+while read pkg
+do
+	brew install $pkg
+done <<EOL
+ansible
+bsdmake
+cmake
+coreutils
+corkscrew
+ctags
+curl
+docker
+dfu-programmer
+gettext
+git
+git-flow
+gnu-sed
+gnu-tar
+go
+gobject-introspection
+groff
+harfbuzz
+htop-osx
+hub
+imagemagick
+innotop
+lynx
+memcached
+mongodb
+msgpack
+mysql
+ngrep
+nikto
+nkf
+nmap
+openldap
+openssh
+openssl
+packer
+percona-toolkit
+perl-build
+phantomjs
+pidof
+plenv
+proctools
+pstree
+pyenv
+rdesktop
+reattach-to-user-namespace
+redis
+rename
+rpm2cpio
+sqlite
+subversion
+the_silver_searcher
+tig
+tree
+wakeonlan
+watch
+wget
+EOL
+
+
+# need compile
+brew install vim --with-lua
+
+while read pkg
+do
+	brew install $pkg
+done <<EOL
+osx-cross/avr/avr-libc
+gcc-arm-none-eabi
 EOL
