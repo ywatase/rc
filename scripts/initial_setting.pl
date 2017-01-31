4a3a3a563e0656a9fe54619756c5b697d8e821f7
Yusuke Watase <ywatase@gmail.com>
2017-02-01 08:29:06 +0900

    remove neobundle

--- scripts/initial_setting.pl
+++ scripts/initial_setting.pl
@@ -78,7 +78,6 @@ sub main {
     _symlink_dot_files();
     _add_zshenv($hash{'.zsh'});
 #    _setup_autojump();
-    _clone_neobundle();
 }
 
 #################################################
@@ -105,19 +104,6 @@ END
     open my $fh, '>>', "$ENV{HOME}/.zshenv" or die $!;
     print $fh $str;
     close $fh;
-}
-
-=item B<_clone_neobundle> - clone neobundle
-
-=cut
-
-sub _clone_neobundle {
-    my $dir = getcwd;
-    system "mkdir $ENV{HOME}/.vim/bundle";
-    chdir "$ENV{HOME}/.vim/bundle";
-
-    system "git clone https://github.com/Shougo/neobundle.vim.git";
-    chdir $dir
 }
 
 =item B<_clone_submodules> - clone submodules
